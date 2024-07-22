2be90c158b6342906504218299e4b0839d855afa
Thandramanuteja <manutejathandra12@gmail.com>
2024-07-22 15:41:40 +0530

    contax

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -9,18 +9,27 @@ import AddToList from './components/20-06-2024/AddToList';
 import Alertmssg from './components/20-06-2024/tasks/Alertmssg';
 import Deleteopt from './components/20-06-2024/tasks/Deleteopt';
 import AlbumId from './components/24-06-2024/task2/AlbumId';
-import ComponentA from './components/24-06-2024/ComponentA';
+// import ComponentA from './components/24-06-2024/ComponentA';
 import Conditionempdata from './components/24-06-2024/task1/Conditionempdata';
 import CondationAlRendering from './components/24-06-2024/CondationAlRendering';
 import RectForgment from './components/26-06-2024/RectForgment';
 import MyFormComponent from './components/tasks/MyFormComponent';
 import UseEffectHook from './components/17-07-2024(use effects)/UseEffectHook';
 import UseEffectApiCall from './components/17-07-2024(use effects)/UseEffectApiCall';
+import ApiSearchData from './components/18-07-2024(api search data)/ApiSearchData';
+import ComponentA from './components/19-07-2024(contax hook)/ComponentA';
+
+import { ContextApp1,ContextApp2 } from './components/19-07-2024(contax hook)/ContextApp';
 
 
 function App() {
+  const empList = [{ name: 'Sam', age: 28, city: 'Hyderbad', id: 100 },
+    { name: 'Ram', age: 24, city: 'Bangalore', id: 100 },
+    { name: 'Siva', age: 25, city: 'Pune', id: 100 }]
   return(
     <div>
+       <ContextApp1.Provider value={empList}>
+       <ContextApp2.Provider value={'Hello World'}>
        {/* <Functioncm/> */}
        {/* <StateEvents/> */}
        {/* <Counter/> */}
@@ -49,7 +58,13 @@ function App() {
        */}
        {/* <MyFormComponent/> */}
        {/* <UseEffectHook/> */}
-       <UseEffectApiCall/>
+       {/* <UseEffectApiCall/> */}
+       {/* <ApiSearchData/> */}
+       
+       <ComponentA/>
+       </ContextApp2.Provider>
+       </ContextApp1.Provider>
+       
     </div>
   )
 }
---
+++ myfirstreact/src/components/19-07-2024(contax hook)/ComponentB.js
@@ -0,0 +1,11 @@
+function ComponentB (){
+    return(
+        <div>
+            <h1>ComponentB</h1>
+        </div>
+        
+
+    )
+}
+
+export default ComponentB;
