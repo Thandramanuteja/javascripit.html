2c5bbe8c6badada079825bbd8c5b5edb6a48c762
Thandramanuteja <manutejathandra12@gmail.com>
2026-01-28 14:11:25 +0530

    files

---
+++ myfirstreact/src/components/Counter-24-07-2024/LocalCounter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "../19-06-2024(functions)/Counter";
+
+function LocalCounter() {
+  return (
+    <div>
+      <h2>Local Counter Component</h2>
+      <Counter />
+    </div>
+  );
+}
+
+export default LocalCounter;
